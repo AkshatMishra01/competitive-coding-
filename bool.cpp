348609cc1be48e17789aa1d4c12711d1dcb47d9d
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2021-11-06 10:49:15 +0530

    Boolean expressions and datatype

---
+++ bool.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool cisboring = true;
+	bool ciswaste = false;
+	cout<<"Is c boring?: "<<cisboring<<endl; //returns 1
+	cout<<"Is c useless?:"<<ciswaste<<endl; //returns 0
+	
+	int x,y;
+	x= 10,y=20;
+	cout<<(x>y)<<endl; //0
+	cout<<(x<y)<<endl; //1
+	cout<<(x == 10)<<endl; //1
+	cout<<(x == 20)<<endl; //0
+	return 0;
+}
