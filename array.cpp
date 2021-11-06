fcce27780979c24be6b71df232fb1097c2859d2f
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2021-11-07 00:43:19 +0530

    Basic Arrays in c++

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[5],i;
+	cout<<"Interger size:"<<sizeof(int)<<endl;
+	for(i=0;i<5;i++){
+		cout<<"Address arr["<<i<<"] is"<<&arr[i]<<endl;
+		
+	}
+	for(i=0;i<5;i++)
+	{
+		cin>>i[arr];
+	}
+	return 0;
+}
