7a37b070a7577d17b28115a15579bfb7daac2a9a
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2021-11-06 14:38:00 +0530

    Nested if else

---
+++ ifelse.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	a = 20,b = 20;
+	
+	if(a < b)
+	{
+		cout<<"A is smaller than B";
+	}
+	else if(b < a)
+	{
+		cout<<"B is smaller than A";
+	}
+	else
+	{
+		cout<<"A is equal to B";
+	}
+	
+}
