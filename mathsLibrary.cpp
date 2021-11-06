266eea57a4cd87df1f0870c920de603e3a8a3ed3
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2021-11-06 10:36:47 +0530

    cmath library usage in c++

---
+++ mathsLibrary.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int a = 10,b = 20;
+	cout<<max(a,b)<<endl;
+	cout<<min(a,b)<<endl;
+	
+	cout<<sqrt(64)<<endl;
+	cout<<round(3.1)<<endl;
+	cout<<round(6.9)<<endl;
+	cout<<log(10000)<<endl;
+	return 0;
+}
