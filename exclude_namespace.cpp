041fc07a970394ca98c403ef7708863d1554b9e8
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2021-11-06 10:09:11 +0530

    Excluding namespace from a c++ code

---
+++ exclude_namespace.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	std::string txt = "Akshat";
+	std::string second;
+	std::cin>>second;
+	std::cout<<"the full name is: "<<txt.append(second);
+}
