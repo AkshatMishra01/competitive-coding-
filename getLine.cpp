9fecc043073c1b77ccb2f9397f36e82ee10b46e9
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2021-11-06 09:39:20 +0530

    user input using the getline function in c++

---
+++ getLine.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+string fullname;
+
+cout<<"Enter the Full Name of the person:"<<endl;
+getline(cin,fullname);
+cout<<"Your name is:"<<fullname;
+}
